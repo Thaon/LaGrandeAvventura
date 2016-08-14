2d8f0aba4e0b79a3ab7d7b6180eb1faa19946761
YAP! <gabriele.maddaloni@gmail.com>
2016-08-14 14:44:52 +0200

    Working commit as starting point

---
+++ Assets/Code/POVSwapper.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class RendererExtensions
+{
+    public static bool IsVisibleFrom(this Renderer renderer, Camera camera)
+    {
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(camera);
+        return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
+    }
+}
+
+public class POVSwapper : MonoBehaviour {
+
+    #region member variables
+
+    private Renderer m_renderer;
+    private enum States { inactive, active, changed };
+    private States m_state = States.inactive;
+
+    #endregion
+
+    void Start ()
+    {
+        m_renderer = GetComponent<Renderer>();
+	}
+	
+	void Update ()
+    {
+        if (m_renderer.IsVisibleFrom(Camera.main) && (m_state == States.inactive || m_state == States.changed))
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+}
